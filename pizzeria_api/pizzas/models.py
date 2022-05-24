3d6802df3b1de4301dc8cd93249d6661a8bd8440
Jonattan Miranda <61204086+jonydavid@users.noreply.github.com>
2022-05-23 22:21:34 -0400

    Configuracion de settings y creacion de tablas

--- pizzeria_api/pizzas/models.py
+++ pizzeria_api/pizzas/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+CATEGORIAS = (
+    ('Basico', 'Basico'),
+    ('Premium', 'Premium')
+)
+
+class Ingrediente(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=60,blank=True, null=True)
+    categoria = models.CharField(choices=CATEGORIAS, null=True, max_length=50)
+
+class Pizza(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50,blank=True, null=True)
+    precio = models.IntegerField(blank=True, null=True)
+    estado = models.CharField(max_length=15,blank=True, null=True)
+    ingrediente = models.ManyToManyField(to=Ingrediente)
+
+    def get_pizza_detail(self):
+        return (ingrediente.nombre for ingrediente in self.ingrediente.all())
+
+    def get_total_ingredientes(self):
+        return self.ingrediente.all().count()
--- pizzeria_api/settings.py
+++ pizzeria_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'pizzeria_api.pizzas',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
